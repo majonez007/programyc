dbfc5de07b784d672efc5eaab18bf308deedac75
majonez007 <maj0nez@poczta.fm>
2017-11-02 12:26:16 +0100

    zlicza slowa

---
+++ zliczaslowa.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#define IN  1 /*wewnatrz slowa*/
+#define OUT 0 /* poza slowem*/
+
+
+/* zlicz  wejsciowe wiersze, slowa i znaki */
+int main()
+{
+int c, nl, nw, nc, state;
+state = OUT;
+nl = nw = nc = 0;
+while ((c = getchar()) != EOF) {
+++nc;
+if (c == 'n')
+	++nl;
+if (c ==' '||c == '\n'|| c == '\t')
+state = OUT;
+else if (state == OUT){
+state = IN;
+++nw;
+}
+}
+printf("%d %d %d\n", nl, nw, nc);
+}
+
+
