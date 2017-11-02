93208acb2f4ffd99d1a49df0644af0b7483394a5
majonez007 <maj0nez@poczta.fm>
2017-11-02 08:42:53 +0100

    przeliczanie temperatury2

---
+++ temp2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/* zestawienie temperatur Fahrenheita-Celsiusza */
+main()
+{
+in fahr;
+
+for (fahr = 0; fahr <= 300; fahr = fahr + 20)
+printf("%3d %6.1\n", fahr, (5.0/9.0)*(fahr-32));
+}
